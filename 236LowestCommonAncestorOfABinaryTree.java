a960047199014a0e5d824c395d94cba5e97eb327
weitianyi1993 <125168186+weitianyi1993@users.noreply.github.com>
2026-09-19 19:22:27 -0700

    Add files via upload

---
+++ 236LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class 236LowestCommonAncestorOfABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return dfs(root, p, q);
+    }
+
+    private TreeNode dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q) {
+            return root;
+        }
+
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode l = dfs(root.left, p, q);
+        TreeNode r = dfs(root.right, p, q);
+        if (l != null && r != null) {
+            return root;
+        } else if (l != null) {
+            return l;
+        } else if (r != null) {
+            return r;
+        } else {
+            return null;
+        }
+    }
+}
