ee1e9a8ae44a075a17f2abf6483d8b06a91dec93
weitianyi1993 <125168186+weitianyi1993@users.noreply.github.com>
2026-09-19 19:07:45 -0700

    Add files via upload

---
+++ 104MaximumDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+class 104MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        return dfs(root);
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
